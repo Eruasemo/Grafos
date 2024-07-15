2ec508d4d427412ea623cff6d57a63cd81a8c30f
MANUEL BALDERAS VICTORIA <manuel.balderas@edu.uag.mx>
2024-07-15 17:12:06 -0600

    Agregar archivos de proyecto.

---
+++ Grafos/Grafo.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    class Grafo
+    {
+        private int vertices;
+        private bool direccion;
+        LinkedList<int>[] adyacencia;
+
+        public Grafo(int vertices, bool direccion)
+        {
+            adyacencia = new LinkedList<int>[vertices];
+            this.vertices = vertices;
+            this.direccion = direccion;
+
+            for (int i = 0; i<adyacencia.Length; i++)
+            {
+                adyacencia[i] = new LinkedList<int>();
+               
+            }
+        }
+
+        public void agregarArco(int v, int w)
+        {
+            adyacencia[v].AddLast(w);
+            if (!direccion)
+            {
+                adyacencia[w].AddLast(v);
+            }
+        }
+
+        private void BPhasta(int v, bool[] visita)
+        {
+            visita[v] = true;
+            Console.Write(v + " ");
+            LinkedList<int> lista = adyacencia[v];
+            foreach(var valor in lista)
+            {
+                if (!visita[valor])
+                {
+                    BPhasta(valor, visita);
+                }
+            }
+        }
+        
+        public void busquedaProfunda(int v)
+        {
+            bool[] visita = new bool[vertices];
+            for(int i=0; i<vertices; i++)
+            {
+                visita[i] = false;
+            }
+
+            BPhasta(v, visita);
+            Console.WriteLine();
+        }
+
+        public void busquedaAncha(int s)
+        {
+            bool[] visita = new bool[vertices];
+            for (int i = 0; i < vertices; i++)
+            {
+                visita[i] = false;
+            }
+
+            Queue<int> cola = new Queue<int>();
+            visita[s] = true;
+            cola.Enqueue(s);
+            while (cola.Any())
+            {
+                s = cola.Dequeue();
+                Console.Write(s + " ");
+                
+
+                LinkedList<int> lista = adyacencia[s];
+
+                foreach(var valor in lista)
+                {
+                    if (!visita[valor])
+                    {
+                        visita[valor] = true;
+                        cola.Enqueue(valor);
+                    }
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Grafos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grafo grafo = new Grafo(7, true);
+            grafo.agregarArco(0, 1);
+            grafo.agregarArco(0, 2);
+            grafo.agregarArco(0, 3);
+            grafo.agregarArco(1, 0);
+            grafo.agregarArco(1, 5);
+            grafo.agregarArco(2, 5);
+            grafo.agregarArco(3, 0);
+            grafo.agregarArco(3, 4);
+            grafo.agregarArco(4, 6);
+            grafo.agregarArco(5, 1);
+            grafo.agregarArco(6, 5);
+
+            Console.WriteLine("Busqueda en profundidad desde vertice 0: ");
+            grafo.busquedaProfunda(0);
+
+            Console.WriteLine("Busqueda a lo ancho desde vertice 0: ");
+            grafo.busquedaAncha(0);
+        }
+    }
+}
